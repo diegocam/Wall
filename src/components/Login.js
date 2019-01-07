d035bedf9d69764284d2fe2474095dcb89ddf32f
Diego Camacho <camachojcd@gmail.com>
2019-01-07 00:17:07 -0800

    Fix React attributes

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,12 +7,12 @@ const Login = () => {
                 <form className="form-signin">
                     <h1 className="h3 mb-3 font-weight-normal">Please log in</h1>
                     <div className="form-group">
-                        <label for="inputEmail" className="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autocomplete="off" />
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autoComplete="off" />
                     </div>
                     <div className="form-group">
-                        <label for="inputPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" autocomplete="off" />
+                        <label htmlFor="inputPassword" className="sr-only">Password</label>
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" autoComplete="off" />
                     </div>
                     <button className="btn btn-primary btn-block" type="submit">Log in</button>
                 </form>
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,24 +7,24 @@ const Register = () => {
                 <form className="form-register">
                     <h1 className="h3 mb-3 font-weight-normal">Register</h1>
                     <div className="form-group">
-                        <label for="inputFirstName" className="sr-only">First Name</label>
-                        <input type="text" id="inputFirstName" className="form-control" placeholder="First Name" autocomplete="off" />
+                        <label htmlFor="inputFirstName" className="sr-only">First Name</label>
+                        <input type="text" id="inputFirstName" className="form-control" placeholder="First Name" autoComplete="off" />
                     </div>
                     <div className="form-group">
-                        <label for="inputLastName" className="sr-only">Last Name</label>
-                        <input type="text" id="inputLastName" className="form-control" placeholder="Last Name" autocomplete="off" />
+                        <label htmlFor="inputLastName" className="sr-only">Last Name</label>
+                        <input type="text" id="inputLastName" className="form-control" placeholder="Last Name" autoComplete="off" />
                     </div>
                     <div className="form-group">
-                        <label for="inputEmail" className="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autocomplete="off" />
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autoComplete="off" />
                     </div>
                     <div className="form-group">
-                        <label for="inputPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" autocomplete="off" />
+                        <label htmlFor="inputPassword" className="sr-only">Password</label>
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" autoComplete="off" />
                     </div>
                     <div className="form-group">
-                        <label for="inputPassword" className="sr-only">Confirm Password</label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" autocomplete="off" />
+                        <label htmlFor="confirmPassword" className="sr-only">Confirm Password</label>
+                        <input type="password" id="confirmPassword" className="form-control" placeholder="Confirm Password" autoComplete="off" />
                     </div>
                     <button className="btn btn-primary btn-block" type="submit">Submit</button>
                 </form>
