da3feafaca2096eee98699b4b163a0dc7ce6d061
Diego Camacho <camachojcd@gmail.com>
2019-01-06 22:46:33 -0800

    Change welcome page text

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -2,9 +2,16 @@ import React from 'react'
 import {Link} from 'react-router-dom' 
 
 const Wall = () => {
-    return <div>
-        <h1>Wall</h1>
-    </div>
+    return (
+        <div className="text-center mt-5">
+            <h2>Welcome to The Wall</h2>
+            <p>
+                The Wall is simply a list of posts made by an individual user. <br/>
+                You may view someone's wall or you may create your own.<br/>
+                Check out some of our <Link to='/users'>user's walls.</Link>
+            </p>
+        </div>
+    )
 }
 
 export default Wall
