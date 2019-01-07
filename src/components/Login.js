47d78eb1a3e9983a3eb0f6e8197039e12c9e98ed
Diego Camacho <camachojcd@gmail.com>
2019-01-06 22:47:07 -0800

    Update Login component html

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Login = () => {
-    return <div>
-        <h1>Login Page</h1>
-    </div>
+    return (
+        <React.Fragment>
+            <div className="col-md-5 offset-md-4 mt-5">
+                <form className="form-signin">
+                    <h1 className="h3 mb-3 font-weight-normal">Please log in</h1>
+                    <div className="form-group">
+                        <label for="inputEmail" className="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autocomplete="off" />
+                    </div>
+                    <div className="form-group">
+                        <label for="inputPassword" className="sr-only">Password</label>
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" autocomplete="off" />
+                    </div>
+                    <button className="btn btn-primary btn-block" type="submit">Log in</button>
+                </form>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default Login
