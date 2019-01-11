5a4c25480fd65f722a528137d065e46120183e18
Diego Camacho <camachojcd@gmail.com>
2019-01-11 01:30:35 -0800

    Provide a proper output to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+var path = require('path')
 
 module.exports = {
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
-                }
+                    loader: 'babel-loader',
+                },
             },
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader!sass-loader",
-                })
-            }
-        ]
+                    fallback: 'style-loader',
+                    use: 'css-loader!sass-loader',
+                }),
+            },
+        ],
     },
     devServer: {
         historyApiFallback: true,
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
+            template: './src/index.html',
+            filename: './index.html',
         }),
         new ExtractTextPlugin('style.css'),
-        new Dotenv()
-    ]
-};
+        new Dotenv(),
+    ],
+}
